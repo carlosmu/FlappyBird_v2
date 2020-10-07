8189596d942bd61795f0f74bb7f2f902fe059f75
carlosmu <carlos.damian.munoz@gmail.com>
2020-10-07 18:31:01 -0300

    Script "Columns" for trigger score.

---
+++ Assets/Scripts/Column.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column : MonoBehaviour
+{
+    // Creamos este método para saber cuándo se activa el trigger del collider de puntos.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            GameController.instance.BirdDie();
+        }
+    }
+}
